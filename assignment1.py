c39849c9b114aa9b124321441be204d03ea1cace
lanshee-world <keyafidel@gmail.com.>
2026-02-04 14:46:10 +0300

    added assignment

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+# python program that is able to calculate the BMI of a person.BMI=(weight)/(height squared)
+weight = 78
+height = 1.75
+basicmetabolicrate = (weight) / (height ** 2)
+print("The basic metabolic rate of the person is: ", basicmetabolicrate)
+
+# area of a rectangle 
+length = 48
+width = 25
+area = length * width
+print("The area of the rectangle is: ", area)
+
+# perimeter
+perimeter = (length * 2) + (width * 2)
+print("The perimeter of the rectangle is: ", perimeter)
---
+++ lesson1d.py
@@ -0,0 +1,6 @@
+# create a python program that is able to calculate the simple interest given the simple interest as 50,000 rate as 5% and time as 8 years.SI is given by(p*r*t)/100
+principle = 50000
+rate = 5
+time = 8
+simpleinterest = (principle * rate * time)/ 100
+print("the simple interest is: ", simpleinterest)
