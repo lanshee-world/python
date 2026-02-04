a4b4abffe586ccea695b2184752eba6eb0be2f54
lanshee-world <keyafidel@gmail.com.>
2026-02-04 13:22:29 +0300

    python add

--- lesson1b.py
+++ lesson1b.py
@@ -2,3 +2,14 @@
 # string: a string is a series of characters inside of quotes
 name ="keya"
 print(name)
+# int:indicates a whole number in python,for an integer we do not use quotes
+age = 39
+print("the age of a person is : ", age)
+print(type(age))
+# float:this is a number with a decimal
+coordinates = 15.4535
+print(coordinates)
+print(type(coordinates))
+
+# research and come up with an example of a complex data type
+
